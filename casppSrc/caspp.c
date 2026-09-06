e6812886350272c8058f9a5ed71bb1e586751b8a
BabyBlueFan <pi_nntw_jo@163.com>
2026-09-06 08:23:35 +0800

    client

--- casppSrc/caspp.c
+++ casppSrc/caspp.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <string.h>
+#include <stdio.h>
 
 int open_client(const char* ip, const char* port)
 {
@@ -10,7 +11,12 @@ int open_client(const char* ip, const char* port)
     memset(&hint, 0, sizeof(hint));
     hint.ai_family = AF_INET;
     hint.ai_socktype = SOCK_STREAM;
-    getaddrinfo(ip, port, &hint, &addrList);
+    int ret = getaddrinfo(ip, port, &hint, &addrList);
+    if (ret != 0)
+    {
+        fprintf(stderr, "getaddinfo 错误!\n");
+        return -1;
+    }
     for (addrptr = addrList; addrptr != NULL; addrptr = addrptr->ai_next)
     {
         fd = socket(addrptr->ai_family, addrptr->ai_socktype, addrptr->ai_protocol);
@@ -25,5 +31,48 @@ int open_client(const char* ip, const char* port)
         fd = -1;
     }
     freeaddrinfo(addrList);
+    return fd;
+}
+
+int open_server(const char* arg_port)
+{
+    int fd = 0;
+    struct addrinfo hint;
+    struct addrinfo* plist;
+    struct addrinfo* ptr;
+
+    memset(&hint, 0, sizeof(hint));
+    hint.ai_family = AF_UNSPEC;
+    hint.ai_socktype = SOCK_STREAM;
+    hint.ai_flags = AI_PASSIVE;
+
+    int res = getaddrinfo(NULL, arg_port, &hint, &plist);
+    if (res != 0) {
+        fprintf(stderr, "In open_server, getaddinfo Error!\n");
+        return -1;
+    }
+    
+    for (ptr = plist; ptr != NULL; ptr = ptr->ai_next) {
+        fd = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+        if (fd < 0) {
+            continue;
+        }
+        
+        if (bind(fd, ptr->ai_addr, ptr->ai_addrlen) == 0)
+        {
+            break;
+        }
+        close(fd);
+    }
+
+    freeaddrinfo(plist);
+
+    if (listen(fd, 1024) != 0)
+    {
+        fprintf(stderr, "listen Error!\n");
+        close(fd);
+        return -1;
+    }
+
     return fd;
 }
--- client/main.c
+++ client/main.c
@@ -9,7 +9,9 @@ int main(int argc, char* argv[])
 {
     char* ip = argv[1];
     char* port = argv[2];
-    int fd = open_client("127.0.0.1", "8888");
+    int fd = open_client("localhost", "8888");
     fprintf(stdout, "fd = %d\n", fd);
+    while (1) {
+    }
     return 0;
 }
